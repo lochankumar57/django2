f035fb1e6f907bb1b6ca2aa61093ea38f1ab53ea
lochankumar57 <125385481+lochankumar57@users.noreply.github.com>
2023-08-25 16:10:59 +0530

    Update settings.py

--- projectk/settings.py
+++ projectk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b_r9dl#brm(-05=92_#psr#%n=djtuy647y23hwb%7py4+$tv$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['15.207.72.152', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['43.205.115.146', 'localhost', '0.0.0.0']
 
 
 # Application definition
